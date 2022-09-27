d8cd2749c77cb6c37d46d9687e2d89a33e5bb62d
aaaa-trsh <47063067+aaaa-trsh@users.noreply.github.com>
2022-09-27 13:17:35 -0700

    Starting on work sections

---
+++ work/chatter/config.js
@@ -0,0 +1,22 @@
+const id = "chatter";
+const path = `./projects/${id}/`;
+
+export default {
+    id: id,
+    title: "Chatter<wbr>.<wbr>dev",
+    type: "work",
+    game: {
+        type: "web",
+        link: "https://chatter.dev/",
+        screenshots: [
+            path + "_jjJ1o.png",
+            path + "iGFyzO.png",
+            path + "kYynSw.png",
+            path + "KSrqd3.png",
+        ],
+    },
+    article: {
+        summary: "Full Stack Dev",
+        description: "Right now I'm building the main Chatter app! In the past I did server-side data collection and processing.\n\nChatter is built on Laravel (PHP), Vue.js, and Python.",
+    },
+};
